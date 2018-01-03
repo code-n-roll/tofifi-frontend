4313088aa4a5f6fd4f65ce24d53ada62e283fef6
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2018-01-03 03:59:43 +0300

    Start implementing user stats

--- app/containers/Orders/OwnerOrderScreen/components/UsersTotalInfo/UserOrder/index.js
+++ app/containers/Orders/OwnerOrderScreen/components/UsersTotalInfo/UserOrder/index.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
 
-class UserOrder extends Component {
-  state = { }
-  render() {
-    const { username, avatarUrl, items, sum, status } = this.props;
-    return (
-      <Card>
-        <CardHeader
-          title={username}
-          avatar={avatarUrl}
-        />
-        <CardText>
-          <div>
-            <ul>
-              {
-                items &&
-                  items.map((item) =>
-                    <li key={item.itemId}>{item.name} {item.price}</li>
-                  )
-              }
-            </ul>
-          </div>
-          <div>
-            {sum} {status}
-          </div>
-        </CardText>
-      </Card>
-    );
-  }
-}
+const UserOrder = (props) => {
+  const { items, user } = props;
+  return (
+    <Card>
+      <CardHeader
+        title={user.username}
+        avatar={user.avatarUrl}
+      />
+      <CardText>
+        <div>
+          <ul>
+            {
+              items &&
+              items.map((item) =>
+                <li key={item.itemId}>{item.info.name} {item.info.price}</li>
+              )
+            }
+          </ul>
+        </div>
+        <div>
+          {user.sum} {user.status}
+        </div>
+      </CardText>
+    </Card>
+  );
+};
 
 UserOrder.propTypes = {
-  username: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string,
+    sum: PropTypes.number,
+    status: PropTypes.number.isRequired,
+  }),
   items: PropTypes.array.isRequired,
-  sum: PropTypes.number,
-  status: PropTypes.number.isRequired,
 };
 
 export default UserOrder;
--- app/containers/Orders/OwnerOrderScreen/components/UsersTotalInfo/index.js
+++ app/containers/Orders/OwnerOrderScreen/components/UsersTotalInfo/index.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
+import { selectStoreItems } from 'pages/DashboardPage/selectors';
 import UserOrder from './UserOrder';
 import './styles.css';
 
-const UsersTotalInfo = (props) => (
-  <div>
-    <div>
-      Need to load data from Roma
-    </div>
-    <div className="submitted-order__user-list">
-      {
-        props.users &&
-          props.users.map((user) =>
-            <UserOrder key={user.userId} {...user} />
-          )
-      }
-    </div>
-  </div>
-);
+class UsersTotalInfo extends Component {
+  getItemsWithInfo = (items) => items.map((item) => {
+    const itemInfo = this.state.storeItems[item.id];
+    if (!itemInfo) {
+      return {
+        ...item,
+        info: {},
+      };
+    }
+
+    return {
+      ...item,
+      info: itemInfo,
+    };
+  })
+
+  render() {
+    return (
+      <div>
+        <div className="submitted-order__user-list">
+          {
+            this.props.users &&
+            this.props.users.map((user) =>
+              <UserOrder
+                key={user.userId}
+                user={user}
+                items={user.items}
+              />
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 UsersTotalInfo.propTypes = {
   users: PropTypes.array.isRequired,
 };
 
-export default UsersTotalInfo;
+const mapStateToProps = (state) => ({
+  storeItems: selectStoreItems(state),
+});
+
+
+export default connect(mapStateToProps)(UsersTotalInfo);
