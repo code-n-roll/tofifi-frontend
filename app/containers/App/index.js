9ffaf37b82a620768f87c34c667354ac2c287920
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2018-01-05 16:48:05 +0300

    Final Anniversary commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,3 +1,12 @@
+/*
+           ,--.
+,--.   ,--.`--' ,---.  ,---.  ,---.  ,--,--.,--. ,--.
+|  |.'.|  |,--.(  .-' | .-. :| .-. |' ,-.  | \  '  /
+|   .'.   ||  |.-'  `)\   --.| '-' '\ '-'  |  \   '
+'--'   '--'`--'`----'  `----'|  |-'  `--`--'.-'  /
+                             `--'           `---'
+*/
+
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
